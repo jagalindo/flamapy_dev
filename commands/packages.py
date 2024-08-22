6ed9af63c533d5991f4cea59a0a380ebb9d668a3
José A. Galindo <jagalindo@us.es>
2024-08-23 01:17:26 +0200

    fix: error in apt

--- commands/packages.py
+++ commands/packages.py
@@ -9,43 +9,42 @@ def pip(ctx):
     """Commands for managing Python dependencies."""
     ctx.ensure_object(dict)
     ctx.obj['PARENT_DIR'] = ctx.obj.get('PARENT_DIR', '')
+    ctx.obj['REPOS'] = ctx.obj.get('REPOS', {})
     pass
+
+def process_directories(ctx, command):
+    parent_dir = ctx.obj['PARENT_DIR']
+    repos = ctx.obj['REPOS']
+    for repo_name in repos.keys():
+        repo_dir = os.path.join(parent_dir, repo_name)
+        setup_path = os.path.join(repo_dir, "setup.py")
+
+        absolute_setup_path = os.path.abspath(setup_path)
+        click.echo(f"Checking: {absolute_setup_path}")
+
+        if os.path.exists(setup_path):
+            click.echo(f"Processing {repo_dir}...")
+            subprocess.run(f"pip {command} .", cwd=repo_dir, shell=True, check=True)
+        else:
+            click.echo(f"{repo_dir} does not contain a setup.py file.")
 
 @click.command()
 @click.pass_context
 def install(ctx):
     """Install dependencies from each directory's setup.py."""
-    parent_dir = ctx.obj['PARENT_DIR']
-    for root, dirs, files in os.walk(parent_dir):
-        if "setup.py" in files:
-            click.echo(f"Installing package in {root}...")
-            subprocess.run(["pip", "install", "."], cwd=root, check=True)
-        else:
-            click.echo(f"No setup.py found in {root}.")
+    process_directories(ctx, "install")
 
 @click.command()
 @click.pass_context
 def update(ctx):
     """Update dependencies from each directory's setup.py."""
-    parent_dir = ctx.obj['PARENT_DIR']
-    for root, dirs, files in os.walk(parent_dir):
-        if "setup.py" in files:
-            click.echo(f"Updating package in {root}...")
-            subprocess.run(["pip", "install", "--upgrade", "."], cwd=root, check=True)
-        else:
-            click.echo(f"No setup.py found in {root}.")
+    process_directories(ctx, "install --upgrade")
 
 @click.command()
 @click.pass_context
 def remove(ctx):
     """Uninstall packages from each directory's setup.py."""
-    parent_dir = ctx.obj['PARENT_DIR']
-    for root, dirs, files in os.walk(parent_dir):
-        if "setup.py" in files:
-            click.echo(f"Uninstalling package in {root}...")
-            subprocess.run(["pip", "uninstall", "-y", "."], cwd=root, check=True)
-        else:
-            click.echo(f"No setup.py found in {root}.")
+    process_directories(ctx, "uninstall -y")
 
 pip.add_command(install)
 pip.add_command(update)
--- flamapy_dev.py
+++ flamapy_dev.py
@@ -4,14 +4,14 @@ import os
 
 # Define the repositories and their URLs
 REPOS = {
-    "flamapy": "https://github.com/flamapy/flamapy.git",
     "flamapy_fw": "https://github.com/flamapy/flamapy_fw.git",
-    "flamapy_rest": "https://github.com/flamapy/flamapy_rest.git",
     "fm_metamodel": "https://github.com/flamapy/fm_metamodel.git",
     "pysat_metamodel": "https://github.com/flamapy/pysat_metamodel.git",
     "bdd_metamodel": "https://github.com/flamapy/bdd_metamodel.git",
-    "flamapy_docs": "https://github.com/flamapy/flamapy_docs.git",
-    "flamapy.github.io": "https://github.com/flamapy/flamapy.github.io.git"
+    "flamapy": "https://github.com/flamapy/flamapy.git",
+    #"flamapy_rest": "https://github.com/flamapy/flamapy_rest.git",
+    #"flamapy_docs": "https://github.com/flamapy/flamapy_docs.git",
+    #"flamapy.github.io": "https://github.com/flamapy/flamapy.github.io.git"
 }
 
 # Define the default parent directory. Current directory is the default.
